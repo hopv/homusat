1001743dbc70a9ea0f1c9cd455b30ec5b9826d3c
Youkichi Hosoi <asicrossedabridgeofdreams@gmail.com>
2019-08-23 14:29:14 +0900

    Test

--- main.ml
+++ main.ml
@@ -103,6 +103,7 @@ let ignore = fun s ->
     let msg = "Warning: unrecognizable argument '" ^ s ^ "' is ignored" in
     Log.prerrln 0 msg
 
+(* main entrance *)
 let _ =
     Arg.parse args ignore usage_msg;
     solve !srcpath
